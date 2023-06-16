c74b9391e59248dcc0c59b9eee96f44a5c9404ca
Eduardo Díaz <eduaedodiaz@gmail.com>
2023-06-16 14:02:19 -0600

    express & servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log("====================================");
-console.log("My app");
-console.log("====================================");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en Express');
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
