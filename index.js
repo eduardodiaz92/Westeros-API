9fcb6eb980747012448521bfd67b35bbd19846a5
Eduardo Díaz <eduaedodiaz@gmail.com>
2023-06-16 19:44:12 -0600

    configurando docker

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en Express');
 });
 
+app.get('/db', (req, res) => {
+  res.json();
+});
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
