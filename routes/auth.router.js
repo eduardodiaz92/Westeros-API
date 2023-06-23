d76c62347e0b41ecda18ea8cb0af55fc9aa97918
Eduardo Díaz <eduaedodiaz@gmail.com>
2023-06-22 18:10:45 -0600

    jwt en servicios

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const { config } = require('../config/config');
 
 const router = express.Router();
-
+const secret = config.jwtSecret;
 router.post(
   '/login',
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
     try {
-      res.status(201).json(req.user);
+      const user = req.user;
+      const payload = {
+        sub: user.id,
+        role: user.role,
+      };
+      const token = jwt.sign(payload, secret);
+      res.status(201).json({ user, token });
     } catch (error) {
       next(error);
     }
